409f79a33173ffdb593d9538eeaf18d0d636f2ab
komal solanki <komal.solanki_cs19@gla.ac.in>
2021-10-17 21:35:09 +0530

    updated

--- Javascript/Assignment-14.js
+++ Javascript/Assignment-14.js
@@ -1,19 +1,14 @@
-let add = document.getElementById('increament');
-let remove = document.getElementById('decreament');
-
-let int = document.getElementsById('number');
-let integer = 0;
-
-
-function increase(){
-    integer= integer+1;
-    int.innerHTML = integer;
-}
-
-function decrease(){
-    integer-= 1;
-    int.innerHTML = integer;
-}
-
-add.addEventListener('click',increase());
-remove.addEventListener('click',decrease());
+var data = 0;
+        document.getElementById("root").innerText=data;
+        function decrement(){
+            if(data == 0){
+                return;
+            }
+            data = data-1;
+            document.getElementById("root").innerText=data;
+        }
+        function increment(){
+            data = data+1;
+            document.getElementById("root").innerText=data;
+        }
+        
